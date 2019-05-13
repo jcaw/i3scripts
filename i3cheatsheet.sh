4def34b4078a2be286557438d5663e85658bfbef
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-13 16:38:13 -0700

    Move from doc comment to internal comment

--- i3cheatsheet.sh
+++ i3cheatsheet.sh
@@ -22,11 +22,10 @@ in_func()
 #
 # We include this here so it can be tweaked in a normal format before being sent
 # to the bash command.
-#
-# This command was developed by trial and error and is thus completely
-# incomprehensible. I am sorry.
 i3cheatsheet()
 {
+    # This command was developed by trial and error and is thus completely
+    # incomprehensible. I am sorry.
     grep -E "^bindsym" ~/.config/i3/config | awk '{$1=""; print $0}' | sed 's/^ *//g' | grep -vE "^XF86" | tr -s ' ' | awk -F: '{ st = index($0," ");print substr($0,0,st-1) "...." substr($0,st+1)}' | sed "s/\$mod/\⊞/g" | sed "s/\+/ /g" | column -t -s '....' -R 1 -W 2 -T 2 -c 80 | column | pr -2 -w 160 -t | less
 }
 
