37eac81cf2944aa8fecaf289973e240e7b58fa52
jcaw <toastedjcaw@gmail.com>
2020-11-17 13:38:04 +0000

    Add script to bring workspace from another screens
    This is tailored to the workflow of having one dominant screen, and
    using the others purely for reference. It assumes you always want the
    focused workspace on the primary screen, so all the commands are
    designed to bring a workspace to the current screen (or swap with
    another).
    
    Without multiple screens, it will just behave like normal i3 and focus
    workspaces unobtrusively.

---
+++ bring_workspace.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys
+from util import (
+    active_screens,
+    focus_output,
+    focus_workspace_number,
+    primary_screen,
+    workspace_screen,
+    workspace_to_screen,
+)
+
+
+def get_next_screen(screen, screens):
+    for screen_ in screens:
+        if screen_ != screen:
+            return screen_
+    return None
+
+
+def _workspace(screen):
+    return screen["current_workspace"]
+
+
+def _name(screen):
+    return screen["name"]
+
+
+if __name__ == "__main__":
+    target_ws_partial = sys.argv[1]
+
+    screens = active_screens()
+
+    primary_screen = primary_screen(screens)
+    primary_ws = _workspace(primary_screen)
+    primary_name = _name(primary_screen)
+    target_screen = workspace_screen(target_ws_partial, screens)
+    # Full workspace name
+    target_ws = target_screen and _workspace(target_screen)
+    target_name = target_screen and _name(target_screen)
+
+    if target_ws == primary_ws:
+        # Focussing the current workspace should cause the screens to flip.
+        next_screen = get_next_screen(primary_screen, screens)
+        workspace_to_screen(primary_ws, _name(next_screen))
+        workspace_to_screen(_workspace(next_screen), primary_name)
+    elif target_screen and primary_screen != target_screen:
+        # Target workspace may be on another screen. If so, we swap workspaces
+        # with the main screen.
+        if target_name:
+            workspace_to_screen(primary_ws, target_name)
+        workspace_to_screen(target_ws, primary_name)
+    else:
+        # Workspace is on the same screen, so just focus it.
+        focus_workspace_number(target_ws_partial)
--- util.py
+++ util.py
@@ -4,6 +4,8 @@ import re
 import logging
 import subprocess as proc
 from collections import namedtuple
+import subprocess
+import json
 
 # A type that represents a parsed workspace "name".
 NameParts = namedtuple('NameParts', ['num', 'shortname', 'icons'])
@@ -38,6 +40,60 @@ def construct_workspace_name(parts):
             new_name += ' ' + parts.icons
 
     return new_name
+
+
+def active_screens():
+    """Get a list of the active screens from i3. (Screens are dicts.)"""
+    result = subprocess.run(["i3-msg", "-t", "get_outputs"], capture_output=True)
+    screens = json.loads(result.stdout)
+    return [s for s in screens if s["active"]]
+
+
+def primary_screen(active):
+    """Get the primary screen (falls back to the first screen)."""
+    for screen in active:
+        if screen["primary"]:
+            return screen
+    # If there's no explicit primary screen, just use the first screen.
+    return active[0]
+
+
+def workspace_screen(workspace, screens):
+    """Get the screen `workspace` is partially on."""
+    for screen in screens:
+        screen_ws = screen["current_workspace"]
+        if workspace in screen_ws:
+            # FIXME: How to handle numbers higher than 9? 10 will match 1, right?
+            return screen
+    return None
+
+
+def workspace_to_screen(workspace, screen):
+    subprocess.run(
+        ["i3-msg", "--", "workspace", "--no-auto-back-and-forth", f"{workspace}"],
+        check=True,
+        capture_output=True,
+    )
+    subprocess.run(
+        ["i3-msg", f"move workspace to output {screen}"],
+        check=True,
+        capture_output=True,
+    )
+
+
+def focus_workspace_number(workspace):
+    """Focus `workspace` on the current screen."""
+    subprocess.run(
+        ["i3-msg", "--", "workspace", f"number {workspace}"],
+        check=True,
+        capture_output=True,
+    )
+
+
+def focus_output(output):
+    return subprocess.run(
+        ["i3-msg", f"focus output {output}"], check=True, capture_output=True
+    )
 
 
 # Return an array of values for the X property on the given window.
